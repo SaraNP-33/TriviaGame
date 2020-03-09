d30d80f13e663f4b576364511c2b7ddd871daa02
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-03-09 13:51:09 -0700

    js commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,9 +8,54 @@ var questions = [{
         {text: 'Columbia', correct: false},
         {text: 'Brazil', correct: true},
         {text: 'Spain', correct: false},
+    ],
+    question:"Which kind of alcohol is Russia nooriously known for? ",
+    answers:[
+        {text: 'Whiskey', correct: false},
+        {text: 'Tequila', correct: false},
+        {text: 'Vodka', correct: true},
+        {text: 'Wine', correct: false},
+    ],
+    question:"Which organ has four chambers? ",
+    answers:[
+        {text: 'The heart', correct: true},
+        {text: 'The kidneys', correct: false},
+        {text: 'The liver', correct: false},
+        {text: 'The Lungs', correct: false},
+    ],
+    question:"What is your body's largest organ? ",
+    answers:[
+        {text: 'The Heart', correct: false},
+        {text: 'The Lungs', correct: false},
+        {text: 'The Brain', correct: false},
+        {text: 'The Skin', correct: true},
+    ],
+    question:"Which mammal has no vocal cords? ",
+    answers:[
+        {text: 'Tiger', correct: false},
+        {text: 'Giraffe', correct: true},
+        {text: 'Orca', correct: false},
+        {text: 'Koala', correct: false},
+    ],
+    question:"How many eyes does a bee have? ",
+    answers:[
+        {text: 'Five', correct: true},
+        {text: 'Two', correct: false},
+        {text: 'Eight', correct: true},
+        {text: 'Three', correct: false},
+    ],
+    question:"What name is used to refer to a group of frogs? ",
+    answers:[
+        {text: 'An Assembly', correct: false},
+        {text: 'A School', correct: false},
+        {text: 'An Army', correct: true},
+        {text: 'A colony', correct: false},
     ]
 }];
 
+//var to strore and tracj correct and wrong answers
 var correctAnswers=0;
 var worngAnswers=0;
 
+//var to keep track of the current question displying
+var count=0;
