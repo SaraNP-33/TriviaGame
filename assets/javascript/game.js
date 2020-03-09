555c733948c25fa73327da01a16ffd6d27568f6c
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-03-09 12:21:35 -0700

    js commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//**********VARIABLES****************/
+
+//object with trivia questions and answers
+var questions = [{
+    question:"Which country produces the most coffee in the world? ",
+    answers:[
+        {text: 'Etiopia', correct: false},
+        {text: 'Columbia', correct: false},
+        {text: 'Brazil', correct: true},
+        {text: 'Spain', correct: false},
+    ]
+}];
+
+var correctAnswers=0;
+var worngAnswers=0;
+
