091b1938c5436e6f0a6c7be303fbfe3091262ffb
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-01-15 19:04:30 -0100

    clicked button start and show the questions in next page

---
+++ assets/javascript/questions.js
@@ -0,0 +1,78 @@
+// const questions = [{
+//     question:"Which country produces the most coffee in the world? ",
+//     answer1:'Etiopia',
+//     answer2: 'Columbia', 
+//     answer3: 'Brazil',
+//     answer4:'Spain', 
+//     correct: "Brazil",
+//     },
+//    { question:"Which kind of alcohol is Russia nooriously known for? ",
+//     answers:[
+//         {text: 'Whiskey', correct: false},
+//         {text: 'Tequila', correct: false},
+//         {text: 'Vodka', correct: true},
+//         {text: 'Wine', correct: false},
+//     ]},
+//     { question:"Which organ has four chambers? ",
+//     answers:[
+//         {text: 'The heart', correct: true},
+//         {text: 'The kidneys', correct: false},
+//         {text: 'The liver', correct: false},
+//         {text: 'The Lungs', correct: false},
+//     ]},
+//     { question:"What is your body's largest organ? ",
+//     answers:[
+//         {text: 'The Heart', correct: false},
+//         {text: 'The Lungs', correct: false},
+//         {text: 'The Brain', correct: false},
+//         {text: 'The Skin', correct: true},
+//     ]},
+//    { question:"Which mammal has no vocal cords? ",
+//     answers:[
+//         {text: 'Tiger', correct: false},
+//         {text: 'Giraffe', correct: true},
+//         {text: 'Orca', correct: false},
+//         {text: 'Koala', correct: false},
+//     ]} ,
+//    {question:"How many eyes does a bee have? ",
+//     answers:[
+//         {text: 'Five', correct: true},
+//         {text: 'Two', correct: false},
+//         {text: 'Eight', correct: true},
+//         {text: 'Three', correct: false},
+//     ]},
+//     { question:"What name is used to refer to a group of frogs? ",
+//     answers:[
+//         {text: 'An Assembly', correct: false},
+//         {text: 'A School', correct: false},
+//         {text: 'An Army', correct: true},
+//         {text: 'A colony', correct: false},
+//     ]}
+// ];
+
+const questions = [{
+    question:"Which country produces the most coffee in the world? ",
+    answer1:'Etiopia',
+    answer2: 'Columbia', 
+    answer3: 'Brazil',
+    answer4:'Spain', 
+    correct: "Brazil",
+    },
+    {
+        question:"Which kind of alcohol is Russia nooriously known for? ",
+        answer1:'Whiskey',
+        answer2: 'Tequila', 
+        answer3: 'Vodka',
+        answer4:'Wine', 
+        correct: "Vodka",
+        },
+     {
+         question:"What is your body's largest organ? ",
+         answer1:'The heart',
+         answer2: 'The Lungs', 
+         answer3: 'The Brain',
+         answer4:'The Skin', 
+         correct: 'The Skin',
+     }]
+
+    
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,24 @@
+let currentQindex=0
+let time=questions.length *15
+
 const startbtn=document.getElementById("start")
 const startCont=document.getElementById("gameControls")
+const questionsContainer=document.getElementById("question-container")
+const qText= document.getElementById("questions")
+const btn1=document.getElementById("btn1")
+const btn2=document.getElementById("btn2")
+const btn3=document.getElementById("btn3")
+const btn4=document.getElementById("btn4")
 
 
-
+function startQuiz(){
+questionsContainer.style.display="block"
+qText.textContent=questions[currentQindex].question
+btn1.textContent=questions[currentQindex].answer1
+btn2.textContent=questions[currentQindex].answer2
+btn3.textContent=questions[currentQindex].answer3
+btn4.textContent=questions[currentQindex].answer4
+}
 
 
 
@@ -19,4 +35,5 @@ const startCont=document.getElementById("gameControls")
 startbtn.addEventListener("click",()=>{
     startCont.style.display="none"
     console.log("click");
+    startQuiz()
 })
