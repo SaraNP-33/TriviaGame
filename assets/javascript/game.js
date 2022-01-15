1f752dcc3c8ed0800b008213b83e1fc6b1074790
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-01-15 14:23:13 -0100

    rebuilding the apps logic with JavaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //**********VARIABLES****************/
-
+$(document).ready(function(){
 //object with trivia questions and answers
 var questions = [{
     question:"Which country produces the most coffee in the world? ",
@@ -8,54 +8,54 @@ var questions = [{
         {text: 'Columbia', correct: false},
         {text: 'Brazil', correct: true},
         {text: 'Spain', correct: false},
-    ],
-    question:"Which kind of alcohol is Russia nooriously known for? ",
+    ]},
+   { question:"Which kind of alcohol is Russia nooriously known for? ",
     answers:[
         {text: 'Whiskey', correct: false},
         {text: 'Tequila', correct: false},
         {text: 'Vodka', correct: true},
         {text: 'Wine', correct: false},
-    ],
-    question:"Which organ has four chambers? ",
+    ]},
+    { question:"Which organ has four chambers? ",
     answers:[
         {text: 'The heart', correct: true},
         {text: 'The kidneys', correct: false},
         {text: 'The liver', correct: false},
         {text: 'The Lungs', correct: false},
-    ],
-    question:"What is your body's largest organ? ",
+    ]},
+    { question:"What is your body's largest organ? ",
     answers:[
         {text: 'The Heart', correct: false},
         {text: 'The Lungs', correct: false},
         {text: 'The Brain', correct: false},
         {text: 'The Skin', correct: true},
-    ],
-    question:"Which mammal has no vocal cords? ",
+    ]},
+   { question:"Which mammal has no vocal cords? ",
     answers:[
         {text: 'Tiger', correct: false},
         {text: 'Giraffe', correct: true},
         {text: 'Orca', correct: false},
         {text: 'Koala', correct: false},
-    ],
-    question:"How many eyes does a bee have? ",
+    ]} ,
+   {question:"How many eyes does a bee have? ",
     answers:[
         {text: 'Five', correct: true},
         {text: 'Two', correct: false},
         {text: 'Eight', correct: true},
         {text: 'Three', correct: false},
-    ],
-    question:"What name is used to refer to a group of frogs? ",
+    ]},
+    { question:"What name is used to refer to a group of frogs? ",
     answers:[
         {text: 'An Assembly', correct: false},
         {text: 'A School', correct: false},
         {text: 'An Army', correct: true},
         {text: 'A colony', correct: false},
-    ]
-}];
+    ]}
+];
 
 //var to strore and tracj correct and wrong answers
-var correctAnswers=0;
-var worngAnswers=0;
+// var correctAnswers=0;
+// var wrongAnswers=0;
 
 //var to track unanswered questions
 
@@ -75,7 +75,7 @@ var count=0;
 //***************FUNCTIONS**************** */
 
 //start with the Start Game function
-$(document).ready(function(){
+
 
 
 // this will clear all the buttons from the html
@@ -86,11 +86,13 @@ $(document).ready(function(){
 function gameStart(){
     $("#start").hide();
     $("#question-container").show();
-    randomQuestion = questions.sort(()=> Math.random() - .5);
-    currentQuestion = 0;
+    
+currentQuestion = 0;
 console.log("started");
+//console.log(questions[0]);
 
 nextQuestion();
+displayQuestion(questions)
 
 }
 
@@ -100,36 +102,44 @@ nextQuestion();
 //add the timer on this function
 
 function nextQuestion(){
-displayQuestion(randomQuestion[currentQuestion])
+if(currentQuestion<questions.length){
+    currentQuestion++
+    
+}
 
 }
 
 //function to determine how the question ans buttons are displayed
 
 function displayQuestion(question){
+    
     $("#questions").text(question.question);
-
     question.answers.forEach(answers => {
-        var button = document.createElement("button")
-        button.innerText= answers.text
-        button.classList.add("btn")
 
-        if(answers.correct){
-            button.dataset.correct=answers.correct
-        };
+        $("#answer-btns").append("<button class='btn answer-choice' correct =" +answers.correct+ ">"+answers.text +"</button>")
+    //     var button = document.createElement("button")
+    //     button.innerText= answers.text
+    //     button.classList.add( "btn","answer-choice")
 
-        console.log(answers.text);
+    //     if(answers.correct){
+            
+    //     };
 
-        $("#answer-btns").append(button)
+    //     console.log(answers.text);
+
+    //     $("#answer-btns").append(button)
        
-    });
+     });
+     
 }
 
 //function to allows us to click on the answer and have it know if
 //it the selection is correct or not
 
-function selectAnswer(){}
-
+function selectAnswer(){
+    console.log("click");
+  
+}
 
 
 //whenever we get to the end of the game we will show how many 
@@ -141,6 +151,20 @@ function resetGame(){
 
 
 $("#start").click(gameStart);
-$("btn").click(selectAnswer);
+$(document).on("click",".answer-choice", function(){
+    var rightAnswer=$(this).attr('correct')
+    //console.log($(this).attr('correct'));
+    console.log(rightAnswer);
+   
+
+    if(rightAnswer == "true"){
+        console.log("this is working")
+        count ++
+        nextQuestion()
+    }
+    else{
+        console.log("something else")
+    }
+});
 
 });
---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+const startbtn=document.getElementById("start")
+const startCont=document.getElementById("gameControls")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+startbtn.addEventListener("click",()=>{
+    startCont.style.display="none"
+    console.log("click");
+})
