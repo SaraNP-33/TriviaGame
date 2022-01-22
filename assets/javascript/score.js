bce7f3e3ca28d31a7c097f58e7bdc1222dba76eb
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-01-22 19:49:30 -0100

    scores displaying on leaderboard page

--- assets/javascript/score.js
+++ assets/javascript/score.js
@@ -1,2 +1,21 @@
 let scores=JSON.parse(localStorage.getItem("highScores")) || []
 console.log(scores)
+
+function displayScore(){
+    const initTr= document.getElementById("initTr")
+    const scoreTr=document.getElementById("scoreTr")
+for (let i=0; i<scores.length; i++){
+    const initTd=document.createElement("td")
+    const scoreTd= document.createElement("td")
+    
+
+    initTd.append(scores[i].initials)
+    scoreTd.append(scores[i].score)
+
+    initTr.append(initTd, scoreTd)
+    // scoreTr.appendChild(scoreTd)
+
+}
+}
+
+displayScore()
