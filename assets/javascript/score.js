c2f9a8eb7f1d5f2b52ccced8059b35b5504784c1
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-01-22 19:55:17 -0100

    fixed the elements to accept more than one core and still display it as a table

--- assets/javascript/score.js
+++ assets/javascript/score.js
@@ -2,18 +2,17 @@ let scores=JSON.parse(localStorage.getItem("highScores")) || []
 console.log(scores)
 
 function displayScore(){
-    const initTr= document.getElementById("initTr")
-    const scoreTr=document.getElementById("scoreTr")
+    const table= document.getElementById("leaderTable")
+    
 for (let i=0; i<scores.length; i++){
     const initTd=document.createElement("td")
     const scoreTd= document.createElement("td")
-    
+    const initTr= document.createElement("tr")
 
     initTd.append(scores[i].initials)
     scoreTd.append(scores[i].score)
-
     initTr.append(initTd, scoreTd)
-    // scoreTr.appendChild(scoreTd)
+    table.appendChild(initTr)
 
 }
 }
